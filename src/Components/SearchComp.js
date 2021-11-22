19ad5c4fe50cfc13e61de4cda90b99aaa18dc12d
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-11-22 19:06:17 -0300

    Requisito 5 completo

--- src/Components/SearchComp.js
+++ src/Components/SearchComp.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
 
 export default class SearchComp extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistSearch: '',
+      validateSearch: true,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.searchClick = this.searchClick.bind(this);
+    this.validateCharacters = this.validateCharacters.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    }, this.validateCharacters);
+  }
+
+  searchClick() {
+    console.log('OK');
+  }
+
+  validateCharacters() {
+    const minChar = 2;
+
+    const {
+      artistSearch,
+    } = this.state;
+
+    if (artistSearch.length >= minChar) {
+      console.log('UEPA');
+      this.setState({
+        validateSearch: false,
+      });
+    } else {
+      this.setState({
+        validateSearch: true,
+      });
+    }
+  }
+
   render() {
+    const { validateSearch, artistSearch } = this.state;
     return (
       <div>
-        <h1>Search Component</h1>
+        <form>
+          <label htmlFor="artistSearch">
+            Digite a banda ou artista a ser pesquisada:
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="artistSearch"
+              value={ artistSearch }
+              onChange={ this.onInputChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ validateSearch }
+            onClick={ this.searchClick }
+          >
+            Procurar
+          </button>
+        </form>
       </div>
     );
   }
