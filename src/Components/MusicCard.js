fe5db2c514eb8ce4dcc295f61b932fe64884ae26
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-11-22 23:00:59 -0300

    Requisito 08 completo mas com erro

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 export default class MusicCard extends Component {
+  constructor() {
+    super();
+
+    this.toggleFavorite = this.toggleFavorite.bind(this);
+
+    this.state = {
+      selectedFavorite: false,
+      loading: false,
+    };
+  }
+
+  async toggleFavorite(music) {
+    const { selectedFavorite } = this.state;
+    if (!selectedFavorite) {
+      this.setState({
+        loading: true,
+        selectedFavorite: true,
+      });
+    }
+    await addSong(music);
+
+    this.setState({
+      loading: false,
+    });
+  }
+
   render() {
     const {
-      music: { trackName, previewUrl },
+      selectedFavorite,
+      loading,
+    } = this.state;
+
+    const { music } = this.props;
+
+    const {
+      music: { trackName, previewUrl, trackId },
     } = this.props;
     return (
-      <div>
-        <h1>{trackName}</h1>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-          .
-        </audio>
-      </div>
-    );
+      loading ? <Loading /> : (
+        <div>
+          <h1>{trackName}</h1>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            <code>audio</code>
+            .
+          </audio>
+          <label htmlFor="favorite">
+            Favorita
+            <input
+              type="checkbox"
+              data-testid={ `checkbox-music-${trackId}` }
+              checked={ selectedFavorite }
+              onChange={ () => this.toggleFavorite(music) }
+            />
+          </label>
+        </div>
+      ));
   }
 }
 
